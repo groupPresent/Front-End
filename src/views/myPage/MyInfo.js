993d033290b2efea8e2d2a60127bbb9fb836ff01
minh0518 <rubics@naver.com>
2022-06-26 16:23:17 +0900

    aa

---
+++ src/views/myPage/MyInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react'
+const MyInfo = () => {
+
+
+    const [info,setInfo]=useState({
+
+    })
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyInfo;
