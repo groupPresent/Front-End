3d4a237331d2f6af402bef429b9bb4197e9ac7a9
minh0518 <rubics@naver.com>
2022-06-27 17:42:05 +0900

    [ADD]메인페이지-기본

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ const App = () => {
         <Route path="/mypage" element={<MyInfo/>}/> {/* 디폴트가 내 정보이므로 */}
 
         {/* <Route path='/mypage/funded' element={}/>
-        <Route path='/mypage/funding' element={}/> */}
+        <Route path='/mypage/funding' element={}/> 
+        받은 펀딩, 보낸펀딩 이것들은 아직 안 만들어놓아서 주석처리*/}
       </Routes>
 
     
