8d19a13387b7b63d4c0092026321df843d3ca7e5
minh0518 <rubics@naver.com>
2022-06-28 21:49:47 +0900

    mypage

--- src/views/myPage/MyPage.js
+++ src/views/myPage/MyPage.js
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Link, Routes, Route } from 'react-router-dom'
-import { useState, useCallback } from 'react'
-import MainList from './pages/MainList'
-
-const MyPage = () => {
-  // const [mainMode, setMainMode] = useState('')
-  
-
-  // let content = null
-
-  // switch (mainMode) {
-  //   case 'READ':
-  //     content=<MyInfo/>
-  //     break
-  //   case 'CREATE': 
-  //     content=<CreateAnniversary setMode={setMode}/>
-  //     break
-
-  //   case 'UPDATE':
-  //     break
-
-  //   case 'DELETE':
-  //     break
-
-  //   default:
-  // }
-
-
-  return (
-    <>
-      <MainList />
-      {/* {content} */}
-      
-    </>
-  )
-}
-
-export default MyPage
-=======
 import { Link } from "react-router-dom";
 import { useState, useCallback } from "react";
 
@@ -257,4 +216,3 @@ const [modalOpen, setModalOpen] = useState(false);
   
   export default MyPage;
   
->>>>>>> 38de3e971e911a6eb44170da512142766413b3b9
