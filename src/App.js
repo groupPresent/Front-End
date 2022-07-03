1f270b645a1d74b217c9fd9026c16bcb807c2c91
neulHa33 <moonn3940@gmail.com>
2022-07-03 20:41:48 +0900

    haneul working

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { useState, useCallback } from "react";
 
 import { Route, Routes } from "react-router-dom";
 import Main from "./views/mainPage/Main";
+import "./styles.css";
+import Friends from "./views/friendPage/Friends";
+import FriendTab from "./views/friendPage/FriendTab";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/" element={<Friends />} />
+        <Route path="/friendtab" element={<FriendTab />} />
       </Routes>
     </>
   );
