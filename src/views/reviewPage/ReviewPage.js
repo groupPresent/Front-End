b42ef3d2ba912d2badd1abd1ecf0f656530352de
yeram-lim <yeramyeye@gmail.com>
2022-07-05 13:28:40 +0900

    ✅[CHORE] 변수이름 변경

--- src/views/reviewPage/ReviewPage.js
+++ src/views/reviewPage/ReviewPage.js
@@ -24,7 +24,7 @@ export default function ReviewPage(props) {
         }]
       }; 
 
-    const API_URL = '/user/funding/review'
+    const REVIEW_API_URL = '/user/funding/review'
     const [mode, setMode] = useState(props.mode); // get, post, update, delete
     const [reviewList, setReviewList] = useState([]);
     const [reviewId, setReviewId] = useState(0);
@@ -37,7 +37,7 @@ export default function ReviewPage(props) {
 
     // //컴포넌트가 불려왔을 때 한 번만 axios 통해 가져온 data를 출력
     useEffect(() => {
-        axios.get(API_URL)    
+        axios.get(REVIEW_API_URL)    
         .then((reviews) => {   
             setReviewList(reviews.reviewList);  
             setMode('get');
@@ -52,17 +52,17 @@ export default function ReviewPage(props) {
 
     if (mode === 'get') {
         return (
-            <ReviewList url={API_URL} reviewList={reviewList} modeEvent={setMode} IdEvent={setReviewId}/>// modeEvent={setMode}
+            <ReviewList url={REVIEW_API_URL} reviewList={reviewList} modeEvent={setMode} IdEvent={setReviewId}/>// modeEvent={setMode}
         )
     } else if (mode === 'post') {
         return (
-            <Write url={API_URL} type={"reivew"}/>// modeEvent={setMode}
+            <Write url={REVIEW_API_URL} type={"reivew"}/>// modeEvent={setMode}
         )
     } else if (mode === 'update') {
         console.log('update')
         console.log(reviewId)
         return (
-            <Write url={API_URL} type={"review"} review={reviewList[reviewId]} reviewModeEvent={setMode}/>
+            <Write url={REVIEW_API_URL} type={"review"} review={reviewList[reviewId]} reviewModeEvent={setMode}/>
         )
     } else if (mode === 'delete') {
         console.log('delete')
