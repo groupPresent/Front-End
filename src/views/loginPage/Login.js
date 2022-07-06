7d4da59571906d2333121286e3e37b1e93df4126
minh0518 <rubics@naver.com>
2022-07-06 20:24:27 +0900

    [ADD]로그인페이지 토큰 세션스토리지에 추가

--- src/views/loginPage/Login.js
+++ src/views/loginPage/Login.js
@@ -1,30 +1,49 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 
 const Login = () => {
-  const [id, setId] = useState('')
-  const [pwd, setPwd] = useState('')
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
 
   const onChangeId = (e) => {
-    setId(e.target.value)
+    setUserName(e.target.value)
   }
 
   const onChangePwd = (e) => {
-    setId(e.target.value)
+    setPassword(e.target.value)
   }
 
   const login = () => {
-    
+    const info=
+    {
+      userName,
+      password
+    }
+      const getToken = async ()=>{
+        try{
+          let response = await axios.post('http://3.34.191.132:8080/user/login',JSON.stringify(info))
+          sessionStorage.setItem('token',response.headers.authorization)
+          console.log(response.headers.authorization)
+        }
+        catch (e){
+          console.log(e)
+        }
+      }
+
+    getToken()
   }
 
   return (
     <div>
-      <input type="text" value={id} onChange={onChangeId}/>
+      <input type="text" value={userName} onChange={onChangeId}/>
       <br/>
-      <input type="text" value={pwd} onChange={onChangePwd} />
+      <input type="text" value={password} onChange={onChangePwd} />
       <br/>
       <button onClick={login}>로그인</button>
       <Link to='/signUp'>회원가입</Link>
+
+    
     </div>
   )
 }
