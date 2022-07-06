717d44124a513960ba579deb9cb16d6fe2f3caa7
minh0518 <rubics@naver.com>
2022-07-06 21:55:49 +0900

    [UPDATE]회원가입 라우터 추가

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import FriendTab from "./views/friendPage/FriendTab";
 import Tab1 from "./views/friendPage/Tab1";
 import Tab2 from "./views/friendPage/Tab2";
 import Login from './views/loginPage/Login'
+import SignUp from './views/signUp/SignUp'
 
 
 
@@ -26,6 +27,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
         <Route path="myPage" element={<MyPage />} />
         <Route path="customerService" element={<CustomerService />} />
         <Route path="receivedfunding" element={<ReceivedFunding />} />
