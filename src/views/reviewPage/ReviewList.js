177c89ec62756627465761cc841ae068c8db9f95
yeram-lim <yeramyeye@gmail.com>
2022-07-09 04:16:08 +0900

    💄[MOD] 리뷰 목록 페이지 css

--- src/views/reviewPage/ReviewList.js
+++ src/views/reviewPage/ReviewList.js
@@ -2,11 +2,11 @@ import React, { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ReviewRow from "./ReviewRow";
 import axios from 'axios';
+import "./ReviewRow.css"
 
 export default function ReviewList(props) {
   return (
     <>
-      <h1>Leview list</h1>
       <ul className="reviewListView">
         {!props.reviewList.length && <h4>선물 후기가 없습니다.</h4>}
         {props.reviewList?.map((review) => {
--- src/views/reviewPage/ReviewRow.js
+++ src/views/reviewPage/ReviewRow.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState, useCallback } from "react";
 import { Route, Routes, Link } from 'react-router-dom';
 import axios from 'axios' 
 import ReviewPage from "./ReviewPage"
+//백그라운드 이미지 경로
+import bg from "../../../src/img/background/withRibon2.png";
+import "../../css/components/box.css"
+import "../../css/components/button.css"
+
 
 export default function ReviewRow(props) {
   const [review, setReview] = useState({});
@@ -12,7 +17,8 @@ export default function ReviewRow(props) {
   }, [props.modeEvent])
 
   useEffect(() => {
-    setReview(props.review)
+    setReview(props.review);
+    document.body.style.background = `url(${bg}) repeat-y center top / cover`;
   }, [limit])
 
   const updateReview = (review) => {
@@ -39,21 +45,28 @@ export default function ReviewRow(props) {
   }
   
   return (
-    <li className="review-row">
-      <div>
-        <div>
-          <button onClick={event => updateReview(review.idx)}>수정</button>
-          {/* <button onClick={event => ReviewPage}>수정</button> */}
-          <button onClick={event => deleteReview(review.idx)}>삭제</button>
-        </div>
-        <div>
-          <img className="gift-img" src={review.photo} />
-        </div>
+    <li className="review-row basic-box">
+      <span className="book-mark"><div></div><div></div></span>
+      <div className="review-box">
         <div className="gift-info">
-          <div className="name">선물 이름: {review.name}</div>
-          <div className="price">선물 가격: {review.price}</div>
-          <div className="star">선물 별점: {review.star}</div>
-          <div className="review">리뷰 내용: {review.review}</div>
+          <div className="gift-img">
+            <img src={review.photo} />
+          </div>
+
+          <div className="gift-detail">
+            <div className="name">선물 이름: {review.name}</div>
+            <div className="price">선물 가격: {review.price}</div>
+            <div className="star">선물 별점: {review.star}</div>
+          </div>
+        </div>
+
+        <div className="review-content">
+          <div>리뷰 내용: {review.review}</div>
+        </div>
+
+        <div className="btn-wrap">
+          <button className="highlight-btn" onClick={event => updateReview(review.idx)}><span>후기 수정</span></button>
+          <button className="highlight-btn" onClick={event => deleteReview(review.idx)}><span>후기 삭제</span></button>
         </div>
       </div>
     </li>
